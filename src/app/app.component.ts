b55ddad78c94af3df8bb7c86e7e30d95bc65e058
mittulmandhan <conanmittul@gmail.com>
2020-01-08 21:56:52 +0530

    after view init and elementref

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'dviate-grid-demo';
+export class AppComponent implements AfterViewInit {
+  constructor(private elementRef: ElementRef) {
+
+  }
+  ngAfterViewInit() {
+    // this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = '#d3d3d3';
+ }
 }
